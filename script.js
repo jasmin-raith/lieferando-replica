06abd09971efd4c1593c9875070203abcddafea3
Jasmin Raith <jasmin-raith@outlook.com>
2023-07-05 12:38:06 +0200

    function add to basket

--- script.js
+++ script.js
@@ -11,3 +11,71 @@ async function includeHTML() {
         }
     }
 }
+
+
+let names = [];
+let prices = [];
+let amounts = [];
+
+
+function addToBasket(name, price) {
+
+    if (names.includes(name)) {
+        let index = names.indexOf(name);
+        amounts[index]++;
+    } else {
+        names.push(name);
+        prices.push(price);
+        amounts.push (1);
+    }
+
+    updateShoppingBasket();
+    renderBasket();
+}
+
+
+function updateShoppingBasket() {
+    let sum = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        sum += prices[i] * amounts[i];
+    }
+
+    document.getElementById('finalSum').innerHTML = sum.toFixed(2).replace('.', ',');
+}
+
+
+function renderBasket() {
+    if (names.length == 0) {
+        
+    } else {
+        let container = document.getElementById('basketContainer');
+        container.innerHTML = ``;
+
+        for (let i = 0; i < names.length; i++) {
+            let name = names[i];
+            let price = prices[i];
+            let amount = amounts[i];
+            container.innerHTML += /*html*/ `
+            <div class="filled-basket">
+                <div class="between-amount-and-button">
+                    <div>
+                        ${amount}x ${name}
+                    </div>    
+                    <div style="display: flex;">
+                        <button class="plus-minus-button">+</button> 
+                        <button class="plus-minus-button">-</button>
+                    </div>
+                </div>
+                <div class="price-and-delete">
+                    <div>
+                        ${price.toFixed(2).replace('.', ',')} €
+                    </div>
+                    <div>
+                        <img src="/img/mülleimer.svg">
+                    </div>
+                </div>
+            </div>`;
+        }
+    }
+}
